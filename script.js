642c1a6dbcb1e0a84ad60649336c195ed7ae6337
Chaitanya306 <98307330+Chaitanya306@users.noreply.github.com>
2025-03-29 14:49:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let firstName = document.getElementsByName("First Name");
+let lastName = document.getElementsByName("Last Name");
+let number = document.getElementsByName("Phone Number");
+let email = document.getElementsByName("Email ID");
+
+function myFunction(){
+	
+  console.log(`First Name: ${firstName[0].value} Last Name: ${lastName[0].value} Phone Number: ${number[0].value} Email ID: ${email[0].value}`)
+}
+
